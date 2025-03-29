cc724d71457d6ba4c1dfcc05e5721d9cef47dfa9
Aakash <aakashrajarnav@gmail.com>
2025-03-30 03:01:47 +0530

    thirtymarch

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -10,3 +10,6 @@ global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
 global using Catalog.API.Data;
+global using Marten.Pagination;
+global using HealthChecks.UI.Client;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
