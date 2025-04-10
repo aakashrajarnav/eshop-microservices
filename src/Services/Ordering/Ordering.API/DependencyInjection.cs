34722cf399351fe9232afaf428e12845312816cd
Aakash <aakashrajarnav@gmail.com>
2025-04-10 22:18:20 +0530

    commit

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,19 +1,18 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
-using Carter;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Configuration;
 
 namespace Ordering.API
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddCarter();
             
-            //services.AddExceptionHandler<CustomExceptionHandler>();
-            //services.AddHealthChecks()
-            //    .AddSqlServer(configuration.GetConnectionString("Database")!);
+            services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("Database")!);
 
             return services;
         }
@@ -22,12 +21,12 @@ namespace Ordering.API
         {
             app.MapCarter();
 
-            //app.UseExceptionHandler(options => { });
-            //app.UseHealthChecks("/health",
-            //    new HealthCheckOptions
-            //    {
-            //        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-            //    });
+            app.UseExceptionHandler(options => { });
+            app.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
 
             return app;
         }
